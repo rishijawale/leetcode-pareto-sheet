211eb5eff05853d07926ece13723a93442e3ae79
Rushikesh Jawale <107312636+rishijawale@users.noreply.github.com>
2026-08-06 23:14:39 +0530

    Time: 0 ms (100%), Space: 20.4 MB (40.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -34,14 +34,25 @@ class Solution:
         #     else:
         #         i+=1
 
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1, len(nums)):
 
-                # if i == j:
-                #     return [i, j+1]
+        #         # if i == j:
+        #         #     return [i, j+1]
 
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+
+        num_to_index= {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_to_index:
+                return [num_to_index[complement] , i]
+
+            num_to_index[num]=i
 
                 
             
